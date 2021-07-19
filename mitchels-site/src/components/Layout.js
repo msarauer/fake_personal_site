fada3e721105e44b87b25e56a1f83eb66195e712
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-07-19 16:44:53 -0600

    When added, styles will be incorportated to home and projects page

--- mitchels-site/src/components/Layout.js
+++ mitchels-site/src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Navbar from "./Navbar"
+import "../styles/global.scss"
 
 const Layout = ({ children }) => {
   return (
--- mitchels-site/src/pages/index.js
+++ mitchels-site/src/pages/index.js
@@ -1,14 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.scss"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>Full Stack Web Developer based in Alberta</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
