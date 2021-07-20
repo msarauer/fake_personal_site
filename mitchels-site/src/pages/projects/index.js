2b8d49fb9a1138546acd225c3cf12dfbfe1de888
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-07-20 14:40:29 -0600

    When added, thumbnails will be created for each project and a project details template is created

--- mitchels-site/src/pages/projects/index.js
+++ mitchels-site/src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.scss"
+import Img from "gatsby-image"
 
 const Projects = ({ data }) => {
   console.log(data)
@@ -17,6 +18,7 @@ const Projects = ({ data }) => {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -41,6 +43,13 @@ export const query = graphql`
           slug
           stack
           title
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         id
       }
