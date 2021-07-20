06245db9cf899b3ecedcce6eb2cd0dec1e795c1a
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-07-19 23:54:11 -0600

    When added, the image is converted to a fluid gatsby image

--- mitchels-site/gatsby-config.js
+++ mitchels-site/gatsby-config.js
@@ -7,6 +7,8 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
     {
@@ -14,6 +16,13 @@ module.exports = {
       options: {
         name: `projects`,
         path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
       },
     },
   ],
